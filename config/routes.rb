546c47a1fc084ef2f7bd091f2aa5ee9e459c5e04
bazz666 <herrera.se1992@gmail.com>
2021-05-24 01:02:09 -0400

    add routes and controller by_date

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  resources :ufs
+
+  get 'by_date/:date', to: 'ufs#by_date'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
