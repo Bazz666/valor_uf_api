b986a6504375abd31721c1606667c8bce90c6084
bazz666 <herrera.se1992@gmail.com>
2021-06-04 01:30:26 -0400

    heroku 3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -45,7 +45,7 @@ Rails.application.configure do
   config.log_level = :debug
 
   # Prepend all log lines with the following tags.
-  config.log_tags = [ :client_ip ]
+  config.log_tags = [ :client_id ]
 
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
